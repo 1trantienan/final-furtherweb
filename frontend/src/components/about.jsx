d6122609ca616445c04fed7544bd7eb0e4a1840c
An Tien Tran <51988270+1trantienan@users.noreply.github.com>
2021-09-04 16:21:49 +0700

    Update About Page

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -45,6 +45,86 @@ export default function About() {
           </a>
         </div>
       </div>
+      <div className="aboutCard about2">
+        <img src={pic2} alt="vandana-Avatar" className="aboutImg" />
+        <div className="aboutContainer">
+          <h4 className="about-name">
+            <b>Tran Viet Anh</b>
+          </h4>
+          <p className="about-task">
+            Task: Cases Status (global View and Country View) and About Page
+          </p>
+          <a
+            href="/"
+            className="about-gitrepo-link"
+          >
+            GitHub
+            <span className="gitExternalLinkIcon">
+              <i className="fas fa-external-link-alt gitExternalLink"></i>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div className="aboutCard about2">
+        <img src={pic2} alt="vandana-Avatar" className="aboutImg" />
+        <div className="aboutContainer">
+          <h4 className="about-name">
+            <b>Tran Viet Anh</b>
+          </h4>
+          <p className="about-task">
+            Task: Cases Status (global View and Country View) and About Page
+          </p>
+          <a
+            href="/"
+            className="about-gitrepo-link"
+          >
+            GitHub
+            <span className="gitExternalLinkIcon">
+              <i className="fas fa-external-link-alt gitExternalLink"></i>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div className="aboutCard about2">
+        <img src={pic2} alt="vandana-Avatar" className="aboutImg" />
+        <div className="aboutContainer">
+          <h4 className="about-name">
+            <b>Tran Viet Anh</b>
+          </h4>
+          <p className="about-task">
+            Task: Cases Status (global View and Country View) and About Page
+          </p>
+          <a
+            href="/"
+            className="about-gitrepo-link"
+          >
+            GitHub
+            <span className="gitExternalLinkIcon">
+              <i className="fas fa-external-link-alt gitExternalLink"></i>
+            </span>
+          </a>
+        </div>
+      </div>
+      <div className="aboutCard about2">
+        <img src={pic2} alt="vandana-Avatar" className="aboutImg" />
+        <div className="aboutContainer">
+          <h4 className="about-name">
+            <b>Tran Viet Anh</b>
+          </h4>
+          <p className="about-task">
+            Task: Cases Status (global View and Country View) and About Page
+          </p>
+          <a
+            href="/"
+            className="about-gitrepo-link"
+          >
+            GitHub
+            <span className="gitExternalLinkIcon">
+              <i className="fas fa-external-link-alt gitExternalLink"></i>
+            </span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
