8377be37061def22469912512604bbcef187cf4b
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-09-16 12:51:56 +0700

    Update route.js

--- frontend/src/components/Register/route.js
+++ frontend/src/components/Register/route.js
@@ -1,1 +1,6 @@
-export const SIGN_UP = "/register";
+export const HOME = "/";
+export const SIGN_UP = "/signup";
+export const LOGIN = "/login";
+export const WEB_APP = "/webapp";
+export const SURVEY = "/survey";
+export const ANSWERS = "/answers";
