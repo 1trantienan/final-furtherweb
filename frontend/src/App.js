1609ce26d961b1aa9b44f548f204620d0509e10a
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-09-18 16:43:34 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,27 +15,38 @@ import  VaccineCompany from './components/vaccineData/vaccineManufacture.jsx';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
+import {ProtectedRoute} from "./components/PrivateRoute";
+import {AuthProvider} from "./components/auth";
 
 
 function App() {
   return (
     <div className="App">
       <div className="content-wrap">
+<<<<<<< Updated upstream
+=======
+        
+        <AuthProvider>
+>>>>>>> Stashed changes
         <Router>
           <HeaderNav />
+          
           <Switch>
             <Route exact path="/" component={Login} />
-            <Route exact path="/home" component={HomePage} />
-            <Route exact path="/vaccination" component={VaccineInfo} />
             <Route exact path="/register" component={register} />
-            <Route exact path="/covidInfo" component={CovidInfo} />
-            <Route exact path="/countryView/:countryName" component={CountryView} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/vaccineCompany" component={VaccineCompany} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/reset" component={Reset} />
+            <ProtectedRoute Redirect to="/dashboard " path="/login">
+              <Route exact path="/dashboard" component={HomePage} />
+              <Route exact path="/vaccination" component={VaccineInfo} />            
+              <Route exact path="/covidInfo" component={CovidInfo} />
+              <Route exact path="/countryView/:countryName" component={CountryView} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/vaccineCompany" component={VaccineCompany} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/reset" component={Reset} />
+            </ProtectedRoute>
           </Switch>
         </Router>
+        </AuthProvider>
       </div>
       <Footer/>
     </div>
