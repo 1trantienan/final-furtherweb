696c57c95d1f98aa8b5e70ecc8d2f15cbd012f21
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-09-18 16:43:14 +0700

    Create PrivateRoute.js

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React, {useContext} from "react";
+import { Route, Redirect } from "react-router-dom";
+import {AuthContext} from "./auth";
+
+export const ProtectedRoute = ({component: RouteComponent,...rest}) => {
+  const {currentUser} = useContext(AuthContext)
+  return (
+    <Route
+      {...rest}
+      render={routeProps => 
+        !!currentUser ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+          <Redirect to={"/login"} />
+        )
+        
+      }
+    />
+  );
+};
