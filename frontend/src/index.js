5773bb013f6a69f0506183c8e85fc85effdba78f
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2021-09-18 16:59:59 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-browser.title = "Covid19"
+document.title = "Covid19"
 
 ReactDOM.render(
   // <React.StrictMode>
